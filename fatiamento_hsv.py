729836652b9f12b7e412774c44422d4a82a4b1cc
vitorgongora <41486681+vitorgongora@users.noreply.github.com>
2023-09-01 19:54:43 -0300

    corrige erro mask, adiciona imagens

--- fatiamento_hsv.py
+++ fatiamento_hsv.py
@@ -1,36 +1,56 @@
+import argparse
 import numpy as np
 import cv2 as cv
 
-
 def main():
-    m = 0       
-    x = 50
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--m", help="Escolha o valor de M (Inteiro 0 <= m <= 360)", required=True)
+    parser.add_argument("-x", "--x", help="Escolha o valor de X (Inteiro 0 <= x <= 179)", required=True)
+    parser.add_argument("-im", "--imagem", help="Defina o nome do arquivo (com extensao) a ser utilizado e contido "
+                                                "na pasta raiz", required=True)
+    args = parser.parse_args()
 
-    if(m+x>360):   
-        intervalo_superior = ((m + x)%360) //2
-        intervalo_inferior= (m - x )//2
+    m = int(args.m)
+    x = int(args.x)
+    filename = args.imagem
 
-    if(m-x<0):
-        intervalo_inferior = (m + x) //2
-        intervalo_superior = ((m - x ) + 360 )//2 
-    else:
-        intervalo_inferior = (m - x )//2 
-        intervalo_superior = (m + x) //2
+    erro = False
+    if m < 0 or m > 360:
+        print("ERRO: Valor de M fora do intervalo permitido")
+        erro = True
+    if x < 0 or x > 179:
+        print("ERRO: Valor de X fora do intervalo permitido")
+        erro = True
+    if not "." in filename:
+        print("ERRO: Arquivo sem extensao definida")
+        erro = True
+    if erro:
+        return 1
 
-    img = cv.imread("circulo.jpg")
+    img = cv.imread(filename)
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     h_channel = hsv_img[:, :, 0]  # Canal H (Matiz)
 
-    mask = np.logical_and(h_channel >= intervalo_inferior, h_channel <= intervalo_superior)
+    if m+x > 360:
+        intervalo_inferior = (m - x) // 2
+        intervalo_superior = ((m + x) % 360) // 2
+        mask = np.logical_and(h_channel <= intervalo_inferior, h_channel >= intervalo_superior)
+    elif m-x < 0:
+        intervalo_inferior = (m + x) // 2
+        intervalo_superior = ((m - x) + 360) // 2
+        mask = np.logical_and(h_channel >= intervalo_inferior, h_channel <= intervalo_superior)
+    else:
+        intervalo_inferior = (m - x) // 2
+        intervalo_superior = (m + x) // 2
+        mask = np.logical_and(h_channel >= intervalo_inferior, h_channel <= intervalo_superior)
 
-    print(h_channel[mask] )
-    h_channel[mask] = (h_channel[mask] + 90 )
+    h_channel[mask] = (h_channel[mask] + 90)
 
     hsv_img[:, :, 0] = h_channel
 
     rgb_img = cv.cvtColor(hsv_img, cv.COLOR_HSV2BGR)    
 
-    cv.imwrite  ("rgb_img_fatia.jpg",rgb_img)
+    cv.imwrite("modificado_" + filename, rgb_img)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
