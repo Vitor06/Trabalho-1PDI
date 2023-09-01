4ce6d635d7761a601f86557b06983989806ab920
Vitor Fernandes <90469090+Vitor06@users.noreply.github.com>
2023-09-01 13:01:58 -0300

    Update fatiamento_hsv.py

--- fatiamento_hsv.py
+++ fatiamento_hsv.py
@@ -3,29 +3,24 @@ import cv2 as cv
 
 
 def main():
-    m = 100
-    x = 10
+    m = 120       
+    x = 4
 
-    if(m+x>360  ):
-        intervalo_superior = (m+x)% 360
-        intervalo_inferior = abs(m-x)
-        #falta o inferior
-    else:
-        intervalo_superior = m+x
-        intervalo_inferior =  m-x
+    intervalo_inferior = (m - x )//2
+    intervalo_superior = (m + x) //2
 
-    img = cv.imread("flor.jpg")
+    img = cv.imread("circulo.jpg")
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     h_channel = hsv_img[:, :, 0]  # Canal H (Matiz)
 
     mask = np.logical_and(h_channel >= intervalo_inferior, h_channel <= intervalo_superior)
-    h_channel[mask] = h_channel[mask] +180
 
-    print(h_channel[mask])
+    h_channel[mask] = (h_channel[mask] + 90)
+
 
     hsv_img[:, :, 0] = h_channel
 
-    rgb_img = cv.cvtColor(hsv_img, cv.COLOR_HSV2BGR)
+    rgb_img = cv.cvtColor(hsv_img, cv.COLOR_HSV2BGR)    
 
     cv.imwrite  ("rgb_img_fatia.jpg",rgb_img)
     cv.waitKey(0)
